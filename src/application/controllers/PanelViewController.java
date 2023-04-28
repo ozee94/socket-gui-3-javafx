6b24d2fc39a69dfa5e1cfbdd608ed255616aa49e
ojiyeon <ojy2333@naver.com>
2023-04-28 09:55:58 +0900

    panel view 분리

---
+++ src/application/controllers/PanelViewController.java
@@ -0,0 +1,21 @@
+package application.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+
+public class PanelViewController implements Initializable{
+
+	@FXML Pane panel5331, panel5332, panel5333, panel5334, panel5335;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+		panel5332.setVisible(false);
+		
+	}
+}
