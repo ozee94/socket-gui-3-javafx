1c80203df34945e3dc141313c1dcf815543628ca
JAVA <ojy2333@gmail.com>
2023-05-03 14:29:23 +0900

    각 fxml의 컨트롤러들을 다루기 위해 ControllerMediator를 생성하여 제어

---
+++ src/application/controllers/ControllerMediator.java
@@ -0,0 +1,40 @@
+package application.controllers;
+
+public class ControllerMediator {
+	
+	private static ControllerMediator instance = null;
+	
+	private PanelViewController panelViewController = null;
+	private TreeViewController treeViewController = null;
+	
+	private ControllerMediator() {}
+	
+	
+	
+	public PanelViewController getPanelViewController() {
+		return panelViewController;
+	}
+
+	public void setPanelViewController(PanelViewController panelViewController) {
+		if(this.panelViewController == null) {
+			this.panelViewController = panelViewController;
+		}
+	}
+
+	public TreeViewController getTreeViewController() {
+		return treeViewController;
+	}
+
+	public void setTreeViewController(TreeViewController treeViewController) {
+		if(this.treeViewController == null) {
+			this.treeViewController = treeViewController;
+		}
+	}
+
+	public static ControllerMediator getInstance () {
+		if(instance == null) {
+			instance = new ControllerMediator();
+		}
+		return instance;
+	}
+}
