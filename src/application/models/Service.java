eea117591b605749846f1110abc93fffc2c55261
JAVA <ojy2333@gmail.com>
2023-05-03 09:05:12 +0900

    .

--- src/application/models/Service.java
+++ src/application/models/Service.java
@@ -6,11 +6,11 @@ import java.util.List;
 import javafx.scene.control.TreeItem;
 
 public enum Service {
-	S5331("119 긴급출동 지원서비스", Arrays.asList("11", "22")), 
-	S5332("재난 상황관리 지원서비스", Arrays.asList("11", "22")),
-	S5333("사회적 약자 지원서비스", Arrays.asList("11", "22")), 
-	S5334("112 긴급출동 지원서비스", Arrays.asList("11", "22")),
-	S5335("112 긴급영상 지원서비스", Arrays.asList("11", "22"));
+	S5331("119 긴급출동 지원서비스", Arrays.asList("11", "21")), 
+	S5332("재난 상황관리 지원서비스", Arrays.asList("12", "22")),
+	S5333("사회적 약자 지원서비스", Arrays.asList("13", "23")), 
+	S5334("112 긴급출동 지원서비스", Arrays.asList("14", "24")),
+	S5335("112 긴급영상 지원서비스", Arrays.asList("15", "25"));
 
 	String service;
 	List<String> category;
