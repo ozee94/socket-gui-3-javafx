efab1e0e211a8cc43532db7d0b5e0444516195c5
JAVA <ojy2333@gmail.com>
2023-05-17 11:27:09 +0900

    Revert "통신별 분리 - 소켓 중 119긴급출동 지원서비스 구현"
    This reverts commit 1ca782d110282a2b8bee66c4516a4e9616defe70.

--- src/application/controllers/ControllerMediator.java
+++ src/application/controllers/ControllerMediator.java
@@ -1,22 +1,21 @@
 package application.controllers;
 
 public class ControllerMediator {
-
+	
 	private static ControllerMediator instance = null;
-
+	
 	private PanelViewController panelViewController = null;
 	private TreeViewController treeViewController = null;
-	private SocketViewController socketViewController = null;
-
-	private ControllerMediator() {
-	}
-
+	
+	private ControllerMediator() {}
+	
+	
 	public PanelViewController getPanelViewController() {
 		return panelViewController;
 	}
 
 	public void setPanelViewController(PanelViewController panelViewController) {
-		if (this.panelViewController == null) {
+		if(this.panelViewController == null) {
 			this.panelViewController = panelViewController;
 		}
 	}
@@ -26,23 +25,13 @@ public class ControllerMediator {
 	}
 
 	public void setTreeViewController(TreeViewController treeViewController) {
-		if (this.treeViewController == null) {
+		if(this.treeViewController == null) {
 			this.treeViewController = treeViewController;
 		}
 	}
-	
-	public SocketViewController getSocketViewController() {
-		return socketViewController;
-	}
-	
-	public void setSocketViewController(SocketViewController socketViewController) {
-		if (this.socketViewController == null) {
-			this.socketViewController = socketViewController;
-		}
-	}
 
-	public static ControllerMediator getInstance() {
-		if (instance == null) {
+	public static ControllerMediator getInstance () {
+		if(instance == null) {
 			instance = new ControllerMediator();
 		}
 		return instance;
--- src/application/controllers/PanelViewController.java
+++ src/application/controllers/PanelViewController.java
@@ -14,25 +14,21 @@ import javafx.scene.layout.Pane;
 
 public class PanelViewController implements Initializable {
 
-//	@FXML Pane panel5331, panel5332, panel5333, panel5334, panel5335;
-	@FXML Pane socketPane, panel5332, panel5334;
-	
-	@FXML SocketViewController socketViewController;
-	
+	@FXML Pane panel5331, panel5332, panel5333, panel5334, panel5335;
 	@FXML Service5331Controller service5331Controller;
 	@FXML Service5332Controller service5332Controller;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		ControllerMediator.getInstance().setSocketViewController(socketViewController);
-
+		// TODO Auto-generated method stub
 //		panel5331.setVisible(false);
 		panel5332.setVisible(false);
-//		panel5333.setVisible(false);
+		panel5333.setVisible(false);
 		panel5334.setVisible(false);
-//		panel5335.setVisible(false);
+		panel5335.setVisible(false);
 	}
 
+	
 	public void handlePanel(TreeItem<String> serviceType) {
 		if(serviceType == null) return ;
 		String parent = serviceType.getParent().getValue();
@@ -43,19 +39,24 @@ public class PanelViewController implements Initializable {
 	}
 	
 	public void setPanel(Service service, String serviceType) {
-		socketPane.setVisible(false);
+		panel5331.setVisible(false); // 119긴급출동 지원서비스
 		panel5332.setVisible(false); // 재난상황 지원서비스
+		panel5333.setVisible(false); // 사회적약자 지원서비스
 		panel5334.setVisible(false); // 112긴급출동 지원서비스
+		panel5335.setVisible(false); // 112긴급영상 지원서비스
 		
 		switch (service.toString()) {
 		case "socket":
-			socketPane.setVisible(true);
+//			panel5331.setVisible(true);			
 			if(Service.socket.getCategory().get(0).equals(serviceType)) {
-				socketViewController.changePane(ServiceType.SOCKET.S_119);
+				panel5331.setVisible(true);
+//				service5332Controller.changePane(ServiceType.DB.ELI_KMA_INFORM);				
 			} else if (Service.socket.getCategory().get(1).equals(serviceType)) {
-				socketViewController.changePane(ServiceType.SOCKET.S_WEAK);
+				panel5333.setVisible(true);
+//				service5332Controller.changePane(ServiceType.DB.ELI_KMA_EARTH_INFM);
 			} else if (Service.socket.getCategory().get(2).equals(serviceType)) {
-				socketViewController.changePane(ServiceType.SOCKET.S_112);
+				panel5335.setVisible(true);
+//				service5332Controller.changePane(ServiceType.DB.ELI_KMA_EARTH_INFM);
 			}
 			break;
 		case "db":
@@ -67,7 +68,7 @@ public class PanelViewController implements Initializable {
 			}
 			break;
 		case "webService":
-//			panel5333.setVisible(true);			
+			panel5333.setVisible(true);			
 			break;
 		default:
 			break;
